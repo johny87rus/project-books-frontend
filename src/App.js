9366828d974bc969d58ef7d166f66183dfa770f4
Evgenii Mikhailov <evgenii@mikhailov.net>
2023-03-31 10:44:53 +0000

    eslint fix (#3)
    Co-authored-by: evgenii <evgenii@mikhailov.net>
    Reviewed-on: https://git.mikhailov.net/evgenii/project-books-frontend/pulls/3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {fetchBooks} from "./thunks/BooksThunk";
 
 function App() {
     const books = useSelector(state => state.books.books);
 
-    const [bookName, setBookName] = useState("");
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(fetchBooks())
-    }, []);
+    }, [dispatch]);
     return (
         <div className="App">
             <div>
             </div>
             <div>
                 <ul>
-                    {books?.map(book => <li>{book.title}</li>)}
+                    {books?.map(book => <li key={book.id}>{book.title}</li>)}
                 </ul>
             </div>
         </div>
