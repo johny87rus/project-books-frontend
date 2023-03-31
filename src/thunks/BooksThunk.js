8989f95b8357d275548fbf10c448b3c601476713
Evgenii Mikhailov <evgenii@mikhailov.net>
2023-03-31 17:31:53 +0000

    fetch url (#4)
    Co-authored-by: Evgenii <mail@mikhailov.net>
    Reviewed-on: https://git.mikhailov.net/evgenii/project-books-frontend/pulls/4

--- src/thunks/BooksThunk.js
+++ src/thunks/BooksThunk.js
@@ -2,6 +2,6 @@ import {createAsyncThunk} from "@reduxjs/toolkit";
 
 
 export const fetchBooks = createAsyncThunk('fetchAllBooks', async () => {
-    const response = await fetch('http://localhost:10001/books');
+    const response = await fetch('https://api.library.mikhailov.net/api/v1/books');
     return await response.json();
 })
